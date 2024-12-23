d995985bd1cf8e892ed50dbd4aa83e4e5782aad8
DaryaKolosova <darya-kolosova@mail.ru>
2024-12-23 19:13:33 +0300

    Start

---
+++ src/main/java/com/example/demo/Hw22ShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw22ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw22ShopApplication.class, args);
+	}
+
+}
